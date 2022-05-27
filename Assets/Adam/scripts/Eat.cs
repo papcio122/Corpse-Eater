569f4d42031d8e591603ee1b73e3a647836f4d19
Adam <adam160590@gmail.com>
2022-05-28 01:28:11 +0200

    test

--- Assets/Adam/scripts/Eat.cs
+++ Assets/Adam/scripts/Eat.cs
@@ -2,12 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.Text.RegularExpressions;
 
 public class Eat : MonoBehaviour
 {
+    public bool eatable = false;
     public Tilemap meat;
     public int points = 0;
     public GameObject general;
+
+    int addpts;
+    string tileName; 
+    public int p_flesh = 1;
+    public int p_bonus_1 = 2;
+    public int p_bonus_2 = 3;
+    public int p_bonus_3 = 4;
+    public int p_bone_w = 10;
+    public int p_bone_v = 20;
+    public int p_bone_b = 30;
+    public int p_bone_g = 40;
 
     // Start is called before the first frame update
     void Start()
@@ -24,14 +37,52 @@ public class Eat : MonoBehaviour
             hitPosition.x = collision.gameObject.transform.position.x;
             hitPosition.y = collision.gameObject.transform.position.y;
 
-            Debug.Log(hitPosition);
-            if (meat.WorldToCell(hitPosition) != null)
+            //Debug.Log(hitPosition);
+            if (meat.WorldToCell(hitPosition) != null && eatable)
             {
+                tileName = meat.GetTile(meat.WorldToCell(hitPosition)).name;
+                ChoosePoints(tileName);
                 meat.SetTile(meat.WorldToCell(hitPosition), null);
-                //points++;
-                general.GetComponent<General>().score++;
+
+                general.GetComponent<General>().score += addpts;
 
             }
+
+        }
+    }
+
+    public void ChoosePoints(string name)
+    {
+        switch (name)
+        {
+            case "FLESH":
+                addpts = p_flesh;
+                break;
+            case "BONUS_1":
+                addpts = p_bonus_1;
+                break;
+            case "BONUS_2":
+                addpts = p_bonus_2;
+                break;
+            case "BONUS_3":
+                addpts = p_bonus_3;
+                break;
+            case var bone when new Regex(@"WBONE\w+").IsMatch(bone):
+                addpts = p_bone_w;
+                break;
+            case var bone when new Regex(@"VBONE\w+").IsMatch(bone):
+                addpts = p_bone_v;
+                break;
+            case var bone when new Regex(@"BBONE\w+").IsMatch(bone):
+                addpts = p_bone_b;
+                break;
+            case var bone when new Regex(@"GBONE\w+").IsMatch(bone):
+                addpts = p_bone_g;
+                break;
+            default:
+                addpts = 0;
+                break;
+
 
         }
     }
